60e3d07c5626f1e11f16300a93ca9a0cd2aecb61
Christophe Laferriere <christophe.laferriere@gmail.com>
2013-03-25 18:38:05 +0100

    forgot routines

--- Local/Routines.pm
+++ Local/Routines.pm
@@ -9,7 +9,8 @@ use Local::Debug qw(debug);
 use Mac::iTunes::Library::XML;
 
 use Exporter qw (import);
-our @EXPORT_OK = qw(version parse get_iso_date get_json_date get_rfc_date);
+our @EXPORT_OK =
+  qw(version parse get_iso_date get_json_date get_rfc_date print_library_header print_library_transition print_playlist_header print_playlist_bottom print_library_bottom);
 
 sub version {
     my $print   = shift;
@@ -55,4 +56,68 @@ sub get_rfc_date {
     my $now = gettimeofday();
     return strftime( "%a, %d %b %Y %H:%M:%S %z", localtime($now) );
 }
+
+# A playlist is composed with :
+# - a header,
+# - a list of track keys,
+# - a transition to playlist,
+#  - a playlist header,
+#  - a list of track id in this playlist,
+#  - a playlist bottom,
+# - a bottom.
+sub print_library_header {
+    print <<EOF;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Major Version</key><integer>1</integer>
+  <key>Minor Version</key><integer>1</integer>
+  <key>Date</key><date>2013-03-23T19:04:19Z</date>
+  <key>Application Version</key><string>11.0.2</string>
+  <key>Features</key><integer>5</integer>
+  <key>Show Content Ratings</key><true/>
+  <key>Music Folder</key><string>file://localhost/Volumes/Black%20Box/Musique/Clean%20iTunes%20Library/iTunes%20Medi\
+a/</string>
+  <key>Library Persistent ID</key><string>02D9E46CD2385F44</string>
+  <key>Tracks</key>
+  <dict>
+EOF
+}
+
+sub print_library_transition {
+    print <<EOF;
+  </dict>
+  <key>Playlists</key>
+  <array>
+EOF
+}
+
+sub print_playlist_header {
+    my $playlist_name = shift;
+    print <<EOF;
+    <dict>
+      <key>Name</key><string>$playlist_name</string>
+      <key>Playlist Items</key>
+      <array>
+        <dict>
+EOF
+}
+
+sub print_playlist_bottom {
+    print <<EOF;
+        </dict>
+      </array>
+    </dict>
+EOF
+}
+
+sub print_library_bottom {
+    print <<EOF;
+  </array>
+</dict>
+</plist>
+EOF
+}
+
 1;
